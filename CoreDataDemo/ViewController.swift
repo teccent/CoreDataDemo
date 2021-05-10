c46f1175bc38ac98ee74e59572e7737c4fbfc31d
Alexey Efimov <lex.efimov@gmail.com>
2021-05-10 22:06:10 +0500

    Initial Commit

---
+++ CoreDataDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataDemo
+//
+//  Created by Alexey Efimov on 10.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
